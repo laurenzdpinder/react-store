c45d7674b875d5654d36c36493757f082adf6f9c
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-01-17 01:53:46 -0300

    feat: add Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <Switch>
+      <Route path="/" component={ Home } />
+    </Switch>
   );
 }
 
