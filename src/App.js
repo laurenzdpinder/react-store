235021e8f024b3e7619b13c6f71a0d374b9ba6f4
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-01-17 02:57:58 -0300

    feat: add Context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { Route, Switch } from "react-router-dom";
+import Provider from "./context/Provider";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 
 function App() {
   return (
-    <>
+    <Provider>
       <Header />
       <Switch>
         <Route path="/productdetails/:id" component={ ProductDetails } />
         <Route path="/" component={ Home } />
       </Switch>
-    </>
+    </Provider>
   );
 }
 
---
+++ src/context/Provider.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyContext from './MyContext';
+
+function Provider({ children }) {
+  const context = {};
+
+  return(
+    <MyContext.Provider value={context}>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+export default Provider;
