6b4ee5aafc36c998d173964da320e07aa6e8381b
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-01-17 02:12:04 -0300

    feat: add ProductDetails page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
   return (
     <>
       <Header />
       <Switch>
+        <Route path="/productdetails/:id" component={ ProductDetails } />
         <Route path="/" component={ Home } />
       </Switch>
     </>
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProductDetails() {
+  return(
+    <h1>Hello ProductDetails!</h1>
+  );
+}
+
+export default ProductDetails;
