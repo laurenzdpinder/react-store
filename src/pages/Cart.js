1ddd8eac60ee310e07a481dd65cb239a019095bd
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-01-25 19:42:51 -0300

    feat: update Cart - tags & localStorage

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getProductsCart } from '../helpers/localStorageCart';
 
 function Cart() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const teste = async () => {
+      const productsCart = await getProductsCart();
+      setProducts(productsCart)
+    }
+    teste();
+  }, [])
+
+  const increaseProductQuantity = () => {
+    return
+  }
+
   return(
-    <h1>Carrinho!</h1>
+    <div>
+      {
+        products.map((product) => {
+          const { id, quantity, title } = product;
+          return (
+            <div key={ `Cart-${id}` }>
+              <h4>{ title }</h4>
+              <div className="d-flex">
+                <button>-</button>
+                <p>{ quantity }</p>
+                <button
+                  onClick={ increaseProductQuantity }
+                  type="button"
+                >
+                  +
+                </button>
+              </div>
+              <p>remover</p>
+            </div>
+          )
+        })
+      }
+      <button type="button">Finalizar Compra</button>
+    </div>
   );
 }
 
