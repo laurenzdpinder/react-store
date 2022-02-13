e614ec8c7a3452f4b7e301584d60bf2af24c3bbb
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-02-12 22:09:30 -0300

    Fix: update useEffect logic to render Loading Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,40 @@
 import React, { useContext, useEffect, useState } from "react";
 import MyContext from '../context/MyContext';
 import { getProductsFromCategoryAndQuery } from '../services/api';
-import ProductCard from '../components/ProductCard';
 import Loading from "../components/Loading";
+import ProductCard from '../components/ProductCard';
+import '../assets/css/Home.css';
 
 function Home() {
+
   const { filters } = useContext(MyContext);
+
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
-  // fetch produts & render Loading component & render ProductCard component(not found answer)
+  // fetch produts & render Loading component & render ProductCard component
   useEffect(()=> {
     const { input, select } = filters;
     const fetchProducts = async () => {
       setLoading(true);
+
       const productsFromAPI = await getProductsFromCategoryAndQuery(select, input);
       setProducts(productsFromAPI);
-      if(productsFromAPI.length === 0) {
-        setLoading(false);
-      }
-    }
-    fetchProducts();
-  }, [filters])
 
-  // render ProductCard component(cards answer)
-  useEffect(()=> {
-    if(products.length > 0) {
       setLoading(false);
-    }
-  },[products])
+    };
+    fetchProducts();
+  }, [filters]);
 
   return(
     <>
-      { loading ?  <Loading /> : <ProductCard products={ products } /> }
+      {
+        loading
+          ?  <Loading />
+          : <div className="home-container">
+              <ProductCard products={ products } />
+            </div>
+      }
     </>
   );
 }
