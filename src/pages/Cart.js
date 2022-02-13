3eea89d59dd62b374cd36a62048d2f29268f2abb
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-02-13 01:53:53 -0300

    feat: update Cart page - product quantituy logic

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import MyContext from '../context/MyContext';
+import Loading from "../components/Loading";
 import { decreaseProductQuantity, increaseProductQuantity, getProductsCart, removeProduct } from '../helpers/localStorageCart';
 import getHdImage from '../helpers/hdImage';
 import '../assets/css/Cart.css';
@@ -7,6 +9,7 @@ import '../assets/css/Cart.css';
 function Cart() {
   const {productsQuantity, setProductsQuantity} = useContext(MyContext);
 
+  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
   // get products from localStorage;
@@ -14,11 +17,15 @@ function Cart() {
   // update totalPrice
   useEffect(() => {
     const retrieveProductsCart = async () => {
+      setLoading(true);
+
       const productsCart = await getProductsCart();
       setProducts(productsCart)
+
+      setLoading(false);
     }
     retrieveProductsCart();
-  }, [productsQuantity])
+  }, [productsQuantity]);
 
   const getTotalOriginalPrice = () => {
     const totalOriginalPrice = products.map(({ original_price, price, quantity }) => {
@@ -37,89 +44,121 @@ function Cart() {
     return `R$ ${totalPrice.toFixed(2)}`;
   }
 
-  return(
-    <div className="cart-container">
-      <div className="cart-product-container">
-      {
-        products.map((product) => {
-          const { id, quantity, original_price, price, thumbnail, title } = product;
-          return (
-            <div
-              className="cart-product"
-              key={ `Cart-${id}` }
-            >
-              <div className="cart-image">
-                <img src={ getHdImage(thumbnail) } alt={title} />
-              </div>
+  const createCartContainer  = () => {
+    if(products.length > 0) {
+      return(
+        <div className="cart-container">
+        <div className="cart-product-container">
+        {
+          products.map((product) => {
+            const { id, quantity, original_price, price, thumbnail, title } = product;
+            return (
+              <div
+                className="cart-product"
+                key={ `Cart-${id}` }
+              >
+                <Link
+                  className="cart-image"
+                  to={ `/productDetails/${id}` }
+                >
+                  <img src={ getHdImage(thumbnail) } alt={title} />
+                </Link>
 
-              <div className="cart-title">
-                <h5>{ title }</h5>
+                <div className="cart-title">
+                  <h5>{ title }</h5>
 
-                <div className="cart-quantity-container">
-                  <h5>Quantidade:</h5>
-                  
-                  <div className="cart-quantity-buttons">
-                    <div className="cart-quantity">
-                      <button
-                        onClick={
-                          () => {
-                            decreaseProductQuantity(id);
-                            setProductsQuantity(productsQuantity - 1);
+                  <div className="cart-quantity-container">
+                    <h5>Quantidade:</h5>
+                    
+                    <div className="cart-quantity-buttons">
+                      <div className="cart-quantity">
+                        <button
+                          onClick={
+                            () => {
+                              decreaseProductQuantity(id);
+                              setProductsQuantity(productsQuantity - 1);
+                            } }
+                            type="button"
+                        >
+                          -
+                        </button>
+
+                        <p>{ quantity }</p>
+
+                        <button
+                          onClick={ () => {
+                            increaseProductQuantity(id);
+                            setProductsQuantity(productsQuantity + 1);
                           } }
                           type="button"
-                      >
-                        -
-                      </button>
-
-                      <p>{ quantity }</p>
+                        >
+                          +
+                        </button>
+                      </div>
 
                       <button
                         onClick={ () => {
-                          increaseProductQuantity(id);
-                          setProductsQuantity(productsQuantity + 1);
+                          removeProduct(id);
+                          setProductsQuantity(productsQuantity - quantity);
                         } }
                         type="button"
                       >
-                        +
+                        remover
                       </button>
                     </div>
-
-                    <button
-                      onClick={ () => {
-                        removeProduct(id);
-                        setProductsQuantity(productsQuantity - quantity);
-                      } }
-                      type="button"
-                    >
-                      remover
-                    </button>
                   </div>
                 </div>
-              </div>
 
-              <div className="subtotal-container">
-                <div className="subtotal-original-price">
-                  { original_price &&
-                    <h5>{ `R$ ${(quantity * original_price).toFixed(2)}` }</h5>
-                  }
+                <div className="subtotal-container">
+                  <div className="subtotal-original-price">
+                    { original_price &&
+                      <h5>{ `R$ ${(quantity * original_price).toFixed(2)}` }</h5>
+                    }
+                  </div>
+                  <h3 className="subtotal-price">{ `R$ ${(quantity * price).toFixed(2)}` }</h3>
                 </div>
-                <h3 className="subtotal-price">{ `R$ ${(quantity * price).toFixed(2)}` }</h3>
               </div>
-            </div>
-          )
-        })
-      }
-      </div>
-
-      <div className="order-summary-container">
-        <div className="order-summary">
-          <h2>resumo do pedido</h2>
-          <h5>{ getTotalOriginalPrice() }</h5>
-          <h3>{ getTotalPrice() }</h3>
-          <button type="button">Finalizar Compra</button>
+            )
+          })
+        }
         </div>
+
+        <div className="order-summary-container">
+          <div className="order-summary">
+            <h2>Resumo do Pedido</h2>
+            <div className="order-summary-price">
+              {
+                productsQuantity > 1
+                  ? <h3>{ `Total (${productsQuantity} itens):` }</h3>
+                  : <h3>{ `Total (${productsQuantity} item):` }</h3>
+              }
+              {
+                getTotalPrice() !== getTotalOriginalPrice()
+                  && <h5>{ getTotalOriginalPrice() }</h5>
+              }
+              <h3>{ getTotalPrice() }</h3>
+            </div>
+            <button type="button">Finalizar Compra</button>
+          </div>
+        </div>
+        </div>
+      );
+    }
+    return (
+      <div className="empty-cart">
+        <h2>Você ainda não possui nenhum produto em seu carrinho de compras!</h2>
       </div>
-    </div>
+    )
+  }
+
+  return(
+    <>
+    {
+      loading
+        ? <Loading />
+        : createCartContainer()
+    }
+    </>
   );
 }
 
