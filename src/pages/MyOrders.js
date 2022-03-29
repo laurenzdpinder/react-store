ddde063cdfebae923dcfe63441d21eaebc3dacb0
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-03-29 05:42:13 -0300

    feat: MyOrders page - get info from localStorage

--- src/pages/MyOrders.js
+++ src/pages/MyOrders.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import { getUsername } from '../helpers/localStorageCart';
+import '../assets/css/MyOrders.css';
 
 function MyOrders() {
+  const [history, setHistory] = useState([{ username: '', orders: '' }]);
+
+  useEffect(() => {
+    const retrieverOrders = async () => {
+      const myOrders = await getUsername();
+      setHistory(myOrders);
+    };
+    retrieverOrders();
+  }, []);
+
+  if (history[0].orders.length) {
+    console.log(history[0].orders.map(({ order }) => order[0].orderNumber));
+  }
+
   return (
     <>
       <Header />
       <div className="my-orders">
-        <h1>Meus Pedidos</h1>
+        {
+          history[0].orders.length
+            ? (
+              <div><h1>Tenho produtos</h1></div>
+            )
+            : (
+              <div><h1>Nao tenho produtos</h1></div>
+            )
+        }
       </div>
     </>
   );
